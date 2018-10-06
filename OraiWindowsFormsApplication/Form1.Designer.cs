4195a3c94319730add15acddd86b3c3f401a417e
Lettike <gati.nikolett@gmail.com>
2018-10-06 19:42:53 +0200

    Órai munka

---
+++ OraiWindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace OraiWindowsFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.dbSzam = new System.Windows.Forms.NumericUpDown();
+            this.ennyiSorLabel = new System.Windows.Forms.Label();
+            this.getAllDataOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dbSzam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(291, 18);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(61, 25);
+            this.buttonPlus.TabIndex = 0;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // dbSzam
+            // 
+            this.dbSzam.Location = new System.Drawing.Point(156, 18);
+            this.dbSzam.Name = "dbSzam";
+            this.dbSzam.Size = new System.Drawing.Size(120, 22);
+            this.dbSzam.TabIndex = 5;
+            // 
+            // ennyiSorLabel
+            // 
+            this.ennyiSorLabel.Location = new System.Drawing.Point(9, 20);
+            this.ennyiSorLabel.Name = "ennyiSorLabel";
+            this.ennyiSorLabel.Size = new System.Drawing.Size(141, 20);
+            this.ennyiSorLabel.TabIndex = 6;
+            this.ennyiSorLabel.Text = "Ennyi sort szeretnék:";
+            // 
+            // getAllDataOut
+            // 
+            this.getAllDataOut.Location = new System.Drawing.Point(358, 20);
+            this.getAllDataOut.Name = "getAllDataOut";
+            this.getAllDataOut.Size = new System.Drawing.Size(75, 23);
+            this.getAllDataOut.TabIndex = 7;
+            this.getAllDataOut.Text = "Mind!";
+            this.getAllDataOut.UseVisualStyleBackColor = true;
+            this.getAllDataOut.Click += new System.EventHandler(this.getAllDataOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(457, 311);
+            this.Controls.Add(this.getAllDataOut);
+            this.Controls.Add(this.ennyiSorLabel);
+            this.Controls.Add(this.dbSzam);
+            this.Controls.Add(this.buttonPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dbSzam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.NumericUpDown dbSzam;
+        private System.Windows.Forms.Label ennyiSorLabel;
+        private System.Windows.Forms.Button getAllDataOut;
+    }
+}
+
---
+++ OraiWindowsFormsApplication/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OraiWindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        private List<TextBox> Text_Boxes = new List<TextBox>();
+        private List<DateTimePicker> Dates = new List<DateTimePicker>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            int db = (int)dbSzam.Value;
+            for (int i = 0; i < db; i++)
+            {
+                TextBox tb = new TextBox();
+                tb.SetBounds(10, 50 + i * 25, 200, tb.Height);
+                this.Controls.Add(tb);
+
+                DateTimePicker dtp = new DateTimePicker();
+                dtp.SetBounds(230, 50 + i * 25, 75, dtp.Height);
+                this.Controls.Add(dtp);
+
+                Button b = new Button();
+                b.Text = "Megjelenít";
+                b.SetBounds(330, 50 + i * 25, 75, b.Height);
+                this.Controls.Add(b);
+                this.Text_Boxes.Add(tb);
+                this.Dates.Add(dtp);
+                b.MouseUp += (sender1, args) => {
+                    if (args.Button == MouseButtons.Left)
+                    {
+                        MessageBox.Show("Tartalom: " + tb.Text + ", " + dtp.Text);
+                    }
+                };
+
+                Button del = new Button();
+                del.Text = "Törlés";
+                del.SetBounds(430, 50 + i * 25, 75, del.Height);
+                del.Click += (sender2, args2) =>
+                {
+                    Text_Boxes.Remove(tb);
+                    this.Controls.Remove(tb);
+                    this.Controls.Remove(b);
+                    this.Controls.Remove(dtp);
+                };
+                this.Controls.Add(del);
+            }
+        }
+
+        private void getAllDataOut_Click(object sender, EventArgs e)
+        {
+            string text = "";
+                foreach (var tb in this.Text_Boxes)
+                {
+                    text += tb.Text + ", ";
+                }
+                foreach (var dtp in Dates)
+                {
+                    text += dtp.Text + "\n";
+                }
+                MessageBox.Show(text);
+                File.AppendAllText("output.txt",text);
+            
+            
+        }
+    }
+}
